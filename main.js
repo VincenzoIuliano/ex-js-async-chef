dbbbf907f4fef62df43a8572196eaa6d243bc46a
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-03-04 10:24:35 +0100

    Refactor getChefBirthday function to use async/await and fetch chef's birth date

--- main.js
+++ main.js
@@ -1,9 +1,21 @@
-function getChefBirthday(id) {
-    fetch(`https://dummyjson.com/recipes/${id}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data);
-        })
+async function getChefBirthday(id) {
+    const recipeResponse = await fetch(`https://dummyjson.com/recipes/${id}`);
+    const recipe = await recipeResponse.json();
+    // console.log(recipe);
+    const chefResponse = await fetch(`https://dummyjson.com/users/${recipe.userId}`);
+    const chef = await chefResponse.json();
+    // console.log(chef);
+    return chef.birthDate; 
 }
 
-getChefBirthday(1);
+(async() => {
+    try{
+        const birthday = await getChefBirthday(1);
+        console.log("Il compleanno dello chef è il:", birthday);
+    } catch(error){
+        console.error(error);
+    }finally{
+        console.log("Fine");
+    }
+})();
+
