101f1ff50a9ee65ccdd4666e61cd9b14d7a27cd5
Vincenzo Iuliano <iuliano-vincenzo@live.it>
2025-03-04 09:59:11 +0100

    Add function to fetch and log recipe data by ID

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log('Ciao, mondo!');
+function getChefBirthday(id) {
+    fetch(`https://dummyjson.com/recipes/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+        })
+}
+
+getChefBirthday(1);
